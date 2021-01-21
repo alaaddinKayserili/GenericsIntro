dce8d31a70d3441c8d3dcff0d7a2f0ef13638628
alaaddinKayserili <77547988+alaaddinKayserili@users.noreply.github.com>
2021-01-22 00:12:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> isimler = new MyList<int>();
+            isimler.Add(10);
+            isimler.Add(20);
+            Console.WriteLine("Eleman sayısı:"+isimler.Length);
+            Console.WriteLine("---------------------");
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+            Console.ReadKey();
+        }
+    }
+}
